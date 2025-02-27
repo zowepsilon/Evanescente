ed4bf2c90911b3b32e7f0e5e0e45d9a2c44b625e
Anatom <49984698+anatom3000@users.noreply.github.com>
2025-02-27 15:31:06 +0100

    remove print

--- utils.py
+++ utils.py
@@ -2,8 +2,6 @@ import functools
 import traceback
 
 def debuggable(f):
-    print(f"wrapping {f}")
-
     @functools.wraps(f)
     async def new(self, ctx, *args, **kwargs):
         try:
@@ -11,7 +9,5 @@ def debuggable(f):
         except Exception as exc:
             if self.bot.config["debug"] and ctx.author.id == self.bot.config["owner"]:
                 await ctx.send(f"Exception lors de l'exécution: ```\n{traceback.format_exc()}```")
-
-    print(f"wrapped {new}")
 
     return new
