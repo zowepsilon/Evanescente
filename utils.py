150ccafb696a09457573e14a1c99e56a8b4e5a59
Anatom <49984698+anatom3000@users.noreply.github.com>
2025-02-27 15:25:15 +0100

    fix await(

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def debuggable(f):
     @functools.wraps(f)
     async def new(self, ctx, *args, **kwargs):
         try:
-            f(self, ctx, *args, **kwargs)
+            await f(self, ctx, *args, **kwargs)
         except Exception as exc:
             if self.bot.config["debug"] and ctx.author.id == self.bot.config["owner"]:
                 await ctx.send(f"Exception lors de l'exécution: ```\n{traceback.format_exc()}```")
