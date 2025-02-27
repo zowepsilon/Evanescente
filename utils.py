e1f7fea1f445a99b39f6cd647afdcef2f0a1fae5
Anatom <49984698+anatom3000@users.noreply.github.com>
2025-02-27 15:26:21 +0100

    fix debuggalbe

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import functools
 import traceback
 
 def debuggable(f):
+    print(f"wrapping {f}")
+
     @functools.wraps(f)
     async def new(self, ctx, *args, **kwargs):
         try:
@@ -9,5 +11,7 @@ def debuggable(f):
         except Exception as exc:
             if self.bot.config["debug"] and ctx.author.id == self.bot.config["owner"]:
                 await ctx.send(f"Exception lors de l'exécution: ```\n{traceback.format_exc()}```")
+
+    print(f"wrapped {new}")
 
     return new
