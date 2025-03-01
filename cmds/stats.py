3d67632bddf7812fa015ac433cc4afaff08a79d4
Anatom <49984698+anatom3000@users.noreply.github.com>
2025-03-01 22:19:48 +0100

    activer pour tout le monde

--- cmds/stats.py
+++ cmds/stats.py
@@ -48,9 +48,6 @@ class Stats(commands.Cog):
         for (_, c) in self.counters.items():
             c.on_message(message)
         
-        if not self.bot.is_dev(message.author.id):
-            return
-
         voc_channel = None
         name = None
 
