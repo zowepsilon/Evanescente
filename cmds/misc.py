0b835065baa1ac75a03ef03320c08df6f785e67f
eliott <107054048+EliottPaq@users.noreply.github.com>
2025-04-09 08:44:21 +0200

    Update misc.py

--- cmds/misc.py
+++ cmds/misc.py
@@ -60,7 +60,7 @@ class Miscellaneous(commands.Cog): # create a class for our cog that inherits fr
     @commands.command()
     @debuggable
     async def fessé(self,ctx,*,target: str = ""):
-        return await ctx.send(f"tu as été.e mauvais.e {target} pour la peine\n"+self.fesse_list[random(0,len(self.fesse_list))])
+        return await ctx.send(f"tu as été.e mauvais.e {target} pour la peine\n"+self.fesse_list[random(0,len(self.fesse_list)-1)])
     
     @commands.command()
     @debuggable
