41fe84b2b0632122a4a7bfb3eb5b11e6e2d8a1f5
Anatom <49984698+anatom3000@users.noreply.github.com>
2025-04-26 20:46:06 +0200

    add startboard test17

--- cmds/starboard.py
+++ cmds/starboard.py
@@ -19,11 +19,12 @@ class Starboard(commands.Cog):
         if reaction is None or reaction.count != 1 or reaction.emoji.id != self.bot.config["starboard_emoji_id"]:
             return
         
+        content = sanitize(message.content)
         time = int(message.created_at.timestamp())
         image = '\n'+message.attachments[0].url if len(message.attachments) > 0 else ""
         
         starboard_channel = self.bot.get_channel(self.bot.config["starboard_id"])
-        await starboard_channel.send(f"> {message.author.mention}\n{message.content}\n\n-# {message.jump_url}{image}")
+        await starboard_channel.send(f"> {message.author.mention}\n{content}\n\n-# {message.jump_url}{image}")
 
 def setup(bot):
     bot.add_cog(Starboard(bot))
