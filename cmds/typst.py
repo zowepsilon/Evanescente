78cc68c83da571f9f76650c2f78e6dc7faf3c6d7
Anatom <49984698+anatom3000@users.noreply.github.com>
2025-05-02 19:08:16 +0200

    add typst edit

--- cmds/typst.py
+++ cmds/typst.py
@@ -36,7 +36,10 @@ class Typst(commands.Cog):
 
         self.renders: dict[int, discord.Message] = {}
 
-    async def process(self, ctx, content: str):
+    async def process(self, ctx, message_id: int, content: str):
+        if message_id in self.renders.keys():
+            await self.renders[message_id].delete()
+
         source = prefix + content
         
         try:
@@ -46,7 +49,8 @@ class Typst(commands.Cog):
             
         except RuntimeError as e:
             reason = e.args[0].replace("`", "​`")
-            return await ctx.send(f"Error while parsing typst:\n```{reason}```")
+            self.renders[message_id] = await ctx.send(f"Error while parsing typst:\n```{reason}```")
+            return
 
         file = discord.File(rendered, "rendered.png")
         self.renders[message_id] = await ctx.send(file=file)
@@ -60,8 +64,10 @@ class Typst(commands.Cog):
                 return await ctx.send("Il faut répondre à un message contenant du code ou donner le code en argument !")
 
             content = (await ctx.fetch_message(ctx.message.reference.message_id)).content
-        
-        await self.process(ctx, content)
+            await self.process(ctx, ctx.message.reference.message_id, content)
+        else:
+            await self.process(ctx, ctx.message.message_id, content)
+
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -69,8 +75,17 @@ class Typst(commands.Cog):
             return
         
         if message.content.count('$') >= 2:
-            await self.process(message.channel, message.content)
+            await self.process(message.channel, message.id, message.content)
+
+    @commands.Cog.listener()
+    async def on_raw_message_edit(self, payload):
+        message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+
+        if message.author.bot or len(message.content) == 0 or message.content[0] == '?':
+            return
         
+        if message.content.count('$') >= 2:
+            await self.process(message.channel, message.id, message.content)
         
 def setup(bot):
     bot.add_cog(Typst(bot))
