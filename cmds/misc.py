dfe517d3f3db7c0ac85a9c4097175160fb6ea4b7
Anatom <49984698+anatom3000@users.noreply.github.com>
2025-06-16 01:45:46 +0200

    add xkcd5

--- cmds/misc.py
+++ cmds/misc.py
@@ -148,7 +148,7 @@ class Miscellaneous(commands.Cog):
         }}'''
 
         async with self.session.post(url, data=data) as req:
-            doc = await req.json()["results"][0]["hits"][0]["document"]
+            doc = (await req.json())["results"][0]["hits"][0]["document"]
             index, title, url, alt = doc["id"], doc["title"], doc["imageUrl"], doc["altTile"]
 
 
