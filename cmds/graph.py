dc51095438dc91bdaeb9e3abbd696c9d14c021d3
Zowε <49984698+zowepsilon@users.noreply.github.com>
2025-08-28 23:20:08 +0200

    fix

--- cmds/graph.py
+++ cmds/graph.py
@@ -13,22 +13,12 @@ class Graph(commands.Cog):
 
         self.db = GraphDb(self.bot.cursor, "MeetingGraph")
 
-    @commands.group(invoke_without_command=True)
-    @debuggable
-    async def graph(self, ctx):
-        rendered = io.BytesIO(self.render())
-
-        file = discord.File(rendered, filename="graph.png")
-        await ctx.send(file=file)
-
-        rendered.close()
-        
-    def render(self) -> bytes:
+    def render(self) -> graphviz.Graph:
         edges = self.db.get_graph()
         nodes = {e[0] for e in edges} | {e[1] for e in edges}
         
         g = graphviz.Graph(engine='neato', format='png')
-        g = graphviz.Graph(format='png')
+        #g = graphviz.Graph(format='png')
 
         for uid in nodes:
             g.node(str(uid), self.bot.nickname_cache.get_nick(uid))
@@ -36,11 +26,25 @@ class Graph(commands.Cog):
         for (u, v) in edges:
             g.edge(str(u), str(v))
 
-        return g.pipe()
+        return g
+
+    @commands.group(invoke_without_command=True)
+    @debuggable
+    async def graph(self, ctx):
+        rendered = io.BytesIO(self.render().pipe())
+
+        file = discord.File(rendered, filename="graph.png")
+        await ctx.send(file=file)
+
+        rendered.close()
 
     @graph.command(name='add')
     @debuggable
     async def graph_add(self, ctx, other: discord.Member):
+        if target.id == ctx.author.id:
+            await ctx.send("Tu ne peux pas ajouter d'arête triviale.")
+            return
+
         user1_nick = sanitize(self.bot.nickname_cache.get_nick(ctx.author.id))
         user2_nick = sanitize(self.bot.nickname_cache.get_nick(other.id))
 
@@ -50,35 +54,14 @@ class Graph(commands.Cog):
             self.db.add_edge(ctx.author.id, other.id)
             await ctx.send(f"L'arête entre {user1_nick} et {user2_nick} a été ajoutée !")
 
-    @debuggable
-    async def _graph(self, ctx, target: discord.Member = None, level: int = None):
-        if target is None or level is None:
-            target = ctx.author.id if target is None else target.id
-            
-            name = sanitize(self.bot.nickname_cache.get_nick(target))
+    @graph.command(name="source")
+    async def graph_source(self, ctx):
+        source = io.StringIO(self.render().source)
 
-            try:
-                level, votes = self.db.get_sanity(target)
-                level = round(level)
-            except ZeroDivisionError:
-                return await ctx.send(f"{name} n'a pas encore de taux de santé mentale !")
+        file = discord.File(source, filename="graph.gv")
+        await ctx.send(file=file)
 
-            return await ctx.send(f"Taux de santé mentale de {name} : {level}% ({votes} estimations)")
-
-        if target.id == ctx.author.id:
-            return await ctx.send("Tu ne peux pas estimer ton propre taux de santé mentale.")
-        
-        if level < 0 or level > 100:
-            return await ctx.send("Le taux de santé mentale doit être compris entre 0 et 100.")
-
-        self.db.change_entry(target.id, ctx.author.id, level)
-
-        name = sanitize(self.bot.nickname_cache.get_nick(target.id))
-        new_level, votes = self.db.get_sanity(target.id)
-        new_level = round(new_level)
-
-        await ctx.send(f"Tu as estimé le taux de santé mentale de {name} à {level}%.\nTaux de santé mentale : {new_level}% ({votes} estimations)")
-
+       source.close()
 
 def setup(bot):
     bot.add_cog(Graph(bot))
