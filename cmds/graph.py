026315851f66ba0047827b62469a9e7f046afc6c
Zowε <49984698+zowepsilon@users.noreply.github.com>
2025-08-31 22:30:19 +0200

    change local layout

--- cmds/graph.py
+++ cmds/graph.py
@@ -28,7 +28,7 @@ class Graph(commands.Cog):
         if center is None:
             g = graphviz.Graph(format='png')
         else:
-            g = graphviz.Graph(format='png', engine="twopi", node_attr={"root": str(center)})
+            g = graphviz.Graph(format='png', engine='twopi', graph_attr={'root': "a", "overlap": "false"})
 
         for uid in nodes:
             g.node(str(uid), self.bot.nickname_cache.get_nick(uid))
