381ad3ae7a7d21e51bddd71f33edf925afa19b6c
Zowε <49984698+zowepsilon@users.noreply.github.com>
2026-01-26 15:47:03 +0100

    check_activity2

--- cmds/inactive.py
+++ cmds/inactive.py
@@ -53,14 +53,9 @@ class Inactive(commands.Cog):
     @debuggable
     async def check_activity(self, ctx, member: discord.Member):
         date = None
-        async for msg in member.history(limit=1):
+        async for msg in member.history(limit=10):
+            await ctx.send(f"Dernier message : {msg}")
             date = msg.created_at
-            break
-
-        if date is None:
-            await ctx.send("Ce membre n'a jamais envoyé de message !")
-        else:
-            await ctx.send(f"Dernier message : {date}")
 
 
 
